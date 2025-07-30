34ccd3745da4a1bf868f3751c532470d22e27e48
Vinicius Costa <vini.cius96554@gmail.com>
2025-07-30 14:13:46 -0300

    implementação e estilização do hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'))
+const imagemin = require('gulp-imagemin')
 
 function styles(){
     return gulp.src('./src/styles/*.scss').pipe(sass({style:'compressed'})).pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images(){
+    return gulp.src('./src/images/**/*', {encoding: false}).pipe(imagemin({verbose: true})).pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
 
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
